af4da6475ae1ca3fed47814d1fdafaa8fe40acbb
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-20 15:40:02 +0530

    Add  src/main/java/com/fetchmessagesapi/fma/FetchMessagesApiApplicationFMA.java

---
+++ src/main/java/com/fetchmessagesapi/fma/FetchMessagesApiApplicationFMA.java
@@ -0,0 +1,19 @@
+package com.fetchmessagesapi.fma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the FetchMessagesAPI Spring Boot application.
+ */
+@SpringBootApplication
+public class FetchMessagesApiApplicationFMA {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchMessagesApiApplicationFMA.class, args);
+	}
+
+}
+```
+```java
+// src/main/java/com/fetchmessagesapi/fma/config/LoggingAspectFMA.java
