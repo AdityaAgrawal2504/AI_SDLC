8d9909e130502cb1543db0a52039fbd96db88bb5
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-20 15:40:09 +0530

    Add  src/main/java/com/fetchmessagesapi/fma/enums/MessageStatusFMA.java

---
+++ src/main/java/com/fetchmessagesapi/fma/enums/MessageStatusFMA.java
@@ -0,0 +1,12 @@
+package com.fetchmessagesapi.fma.enums;
+
+/**
+ * Enumeration for the read/unread status of a message.
+ */
+public enum MessageStatusFMA {
+    read,
+    unread
+}
+```
+```java
+// src/main/java/com/fetchmessagesapi/fma/enums/SortByFMA.java
