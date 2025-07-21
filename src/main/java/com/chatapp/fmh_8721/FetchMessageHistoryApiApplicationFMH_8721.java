f050585e3a2eaef6bd822cd484a7090e887b9bdc
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 14:22:39 +0530

    Add  src/main/java/com/chatapp/fmh_8721/FetchMessageHistoryApiApplicationFMH_8721.java

---
+++ src/main/java/com/chatapp/fmh_8721/FetchMessageHistoryApiApplicationFMH_8721.java
@@ -0,0 +1,16 @@
+package com.chatapp.fmh_8721;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchMessageHistoryApiApplicationFMH_8721 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchMessageHistoryApiApplicationFMH_8721.class, args);
+	}
+
+}
+```
+```java
+// src/main/java/com/chatapp/fmh_8721/logging/StructuredLoggerFMH_8721.java
